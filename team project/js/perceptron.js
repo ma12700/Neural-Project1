0e4b48ca03064ff979955417b6a570d586029c1a
ma12700 <44336948+ma12700@users.noreply.github.com>
2020-04-15 23:32:53 +0200

    Update perceptron.js

--- team project/js/perceptron.js
+++ team project/js/perceptron.js
@@ -1,1 +1,30 @@
-
+function Perceptron(P, T, W, B){
+    var test = 0;
+    var i = 0,e=0,a;
+    while(true){
+        
+        for(var j = 0 ; j < P.length ; j++){
+            if(i == P.length){
+                i = 0;
+            }
+            
+            a=(multiplyMatrix(w,p[i]) + b);
+            
+            if( compare(a,t[i])){
+                i++;
+                test = test + 1;
+            }else{
+                break;
+            }
+        }
+        
+        if(test == p.length){
+            break;
+        }else{
+            test = 0;
+            e = subTwoArray(t[i],a);
+            w = addTwoArray(w,multiplyMatrix(e,p[i]));
+            b = addTwoArray(b,e);
+        }
+    }
+}
